d1923b79ba75df7cce41a2cb7d51ed0840208564
RenYifei <renyifei@onlinegame-13-74.i.netease.com>
2014-09-12 10:57:21 +0800

    init test files

---
+++ sw_client.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstdlib>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <memory.h>
+#include <errno.h>
+
+const char* HOST = "127.0.0.1";
+const int PORT = 10086;
+
+int init()
+{
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_fd <= 0)
+	{
+		printf("create client socket error %s\n", strerror(errno));
+		return -1;
+	}
+
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+
+	if (inet_pton(AF_INET, HOST, &server_addr.sin_addr) <= 0)
+	{
+		printf("inet_pton error for %s, errno:%s\n", HOST, strerror(errno)); 
+		return -1;
+	}
+
+	int connect_res = connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if (connect_res < 0)
+	{
+		printf("connect failed for %s, connect_res %d, errno:%s\n", HOST, connect_res, strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	int sock_fd;
+	if ((sock_fd = init()) < 0)
+	{
+		printf("init failed");
+		return 0;
+	}
+
+	const char* msg = "this is not a hello world.";
+	send(sock_fd, msg, sizeof(msg), 0);
+	
+	return 0;
+}
+
